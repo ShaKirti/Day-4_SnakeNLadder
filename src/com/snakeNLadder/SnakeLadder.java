aa84a197df0e5fdb4901c68aaf4d39348e74b98b
Shashank Mishra <mishra.shashankofficial06@gmail.com>
2022-10-21 02:53:46 +0530

    Added: Single player rolls a die.

--- src/com/snakeNLadder/SnakeLadder.java
+++ src/com/snakeNLadder/SnakeLadder.java
@@ -1,9 +1,20 @@
 package com.snakeNLadder;
 
 public class SnakeLadder {
+    public static int rollCheck() {
+        int rollValue = (int) (Math.floor(Math.random() * 10 % 7));
+        return rollValue;
+
+    }
+
     public static void main(String[] args) {
         int position1 = 0;
-        System.out.println("Snake & Ladder Simulator");
+        System.out.println("Snake and Ladder Simulator");
         System.out.println("Position of Player1 is:" + position1);
+        int rollDie = rollCheck();
+        if (rollDie > 0)
+            System.out.println("Value at die is: " + rollDie);
+        else
+            System.out.println("Roll the die again!");
     }
 }

90fad0506129a8d4ecbf5cacc9d6d42192c0d87c
Shashank Mishra <mishra.shashankofficial06@gmail.com>
2022-10-21 03:05:17 +0530

    Added: Winning position logic.

--- src/com/snakeNLadder/SnakeLadder.java
+++ src/com/snakeNLadder/SnakeLadder.java
@@ -2,22 +2,21 @@ package com.snakeNLadder;
 
 public class SnakeLadder {
     public static int rollCheck() {
-        int rollValue = (int) (Math.floor(Math.random() * 10 % 7));
+        int rollValue = (int) (Math.floor(Math.random() * 10 % 6 + 1));
         return rollValue;
 
     }
-
     public static int options() {
-        int option = (int) (Math.floor(Math.random() * 10 % 3));
+        int option = (int) (Math.floor(Math.random() * 10 % 4));
         return option;
     }
-
     public static void main(String[] args) {
         int position1 = 0;
-        System.out.println("Snake and Ladder Simulator");
+        System.out.println("Welcome to Snake and Ladder Game");
         System.out.println("Position of Player1 is:" + position1);
-        int rollDie = rollCheck();
-        if (rollDie > 0) {
+        int winningPosition = 100;
+        while (position1 != winningPosition) {
+            int rollDie = rollCheck();
             System.out.println("Value at die is: " + rollDie);
             int checkOption = options();
             switch (checkOption) {
@@ -33,12 +32,15 @@ public class SnakeLadder {
                     position1 = position1 - rollDie;
                     if (position1 < 0)
                         position1 = 0;
+                case 3:
+                    if (position1 > 100) {
+                        System.out.println("Sorry The Position is Out of Board.");
+                        position1 -= rollDie;
+                    }
                     break;
+
             }
             System.out.println("Current Position of player1 is: " + position1);
-
-        } else
-            System.out.println("Roll the die again!");
-
+        }
     }
 }

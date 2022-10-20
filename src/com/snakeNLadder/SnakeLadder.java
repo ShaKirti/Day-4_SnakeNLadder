18f19df77603da68f4061934936fb3755c8637a8
Shashank Mishra <mishra.shashankofficial06@gmail.com>
2022-10-21 02:59:26 +0530

    Added: No play, Snake and ladder.

--- src/com/snakeNLadder/SnakeLadder.java
+++ src/com/snakeNLadder/SnakeLadder.java
@@ -7,14 +7,38 @@ public class SnakeLadder {
 
     }
 
+    public static int options() {
+        int option = (int) (Math.floor(Math.random() * 10 % 3));
+        return option;
+    }
+
     public static void main(String[] args) {
         int position1 = 0;
         System.out.println("Snake and Ladder Simulator");
         System.out.println("Position of Player1 is:" + position1);
         int rollDie = rollCheck();
-        if (rollDie > 0)
+        if (rollDie > 0) {
             System.out.println("Value at die is: " + rollDie);
-        else
+            int checkOption = options();
+            switch (checkOption) {
+                case 0:
+                    System.out.println("No play");
+                    break;
+                case 1:
+                    System.out.println("Ladder");
+                    position1 = position1 + rollDie;
+                    break;
+                case 2:
+                    System.out.println("Snake");
+                    position1 = position1 - rollDie;
+                    if (position1 < 0)
+                        position1 = 0;
+                    break;
+            }
+            System.out.println("Current Position of player1 is: " + position1);
+
+        } else
             System.out.println("Roll the die again!");
+
     }
 }

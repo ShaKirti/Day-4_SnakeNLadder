06dc3edca2f4c6d2b4f7c10e3cf6c4c6f4e5a01e
Shashank Mishra <mishra.shashankofficial06@gmail.com>
2022-10-21 02:50:50 +0530

    Added: Single player at position 0.

--- src/com/snakeNLadder/SnakeLadder.java
+++ src/com/snakeNLadder/SnakeLadder.java
@@ -2,6 +2,8 @@ package com.snakeNLadder;
 
 public class SnakeLadder {
     public static void main(String[] args) {
+        int position1 = 0;
         System.out.println("Snake & Ladder Simulator");
+        System.out.println("Position of Player1 is:" + position1);
     }
 }
